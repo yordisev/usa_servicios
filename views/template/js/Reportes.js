09f88f089d96fa9ecd2d87e4288ae4ae2a538af5
yordisev <yordys12@gmail.com>
2022-02-06 21:05:55 -0800

    terminar

--- views/template/js/Reportes.js
+++ views/template/js/Reportes.js
@@ -158,4 +158,87 @@ reportesempleados();
 // chart.render();
 
 
+// var options = {
+//     series: [{
+//         name: 'PRODUCT A',
+//         data: [44, 55, 41, 67, 22, 43]
+//     }, {
+//         name: 'PRODUCT B',
+//         data: [13, 23, 20, 8, 13, 27]
+//     }, {
+//         name: 'PRODUCT C',
+//         data: [11, 17, 15, 15, 21, 14]
+//     }, {
+//         name: 'PRODUCT D',
+//         data: [21, 7, 25, 13, 22, 8]
+//     }],
+//     chart: {
+//         type: 'bar',
+//         height: 350,
+//         stacked: true,
+//         toolbar: {
+//             show: true
+//         },
+//         zoom: {
+//             enabled: true
+//         }
+//     },
+//     responsive: [{
+//         breakpoint: 480,
+//         options: {
+//             legend: {
+//                 position: 'bottom',
+//                 offsetX: -10,
+//                 offsetY: 0
+//             }
+//         }
+//     }],
+//     plotOptions: {
+//         bar: {
+//             horizontal: false,
+//             borderRadius: 10
+//         },
+//     },
+//     xaxis: {
+//         type: 'datetime',
+//         categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+//             '01/05/2011 GMT', '01/06/2011 GMT'
+//         ],
+//     },
+//     legend: {
+//         position: 'right',
+//         offsetY: 40
+//     },
+//     fill: {
+//         opacity: 1
+//     }
+// };
 
+// var chart = new ApexCharts(document.querySelector("#chart"), options);
+// chart.render();
+
+
+
+
+// var options = {
+//     series: [44, 55, 13, 43, 22],
+//     chart: {
+//         width: 380,
+//         type: 'pie',
+//     },
+//     labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+//     responsive: [{
+//         breakpoint: 480,
+//         options: {
+//             chart: {
+//                 width: 200
+//             },
+//             legend: {
+//                 position: 'bottom'
+//             }
+//         }
+//     }]
+// };
+
+// var chart = new ApexCharts(document.querySelector("#chart"), options);
+// chart.render();
